200fde8c025ed447567efa060de9e1e62784bcaa
S0umyajit <soumyajit2k18@gmail.com>
2026-06-03 23:56:29 +0530

    highest avg salary among dept

---
+++ src/test4/DeptWithHighAvgSal.java
@@ -0,0 +1,26 @@
+package test4;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class DeptWithHighAvgSal {
+    public static void main(String[] args) {
+        List<EmployeeNew> employees = Arrays.asList(
+                new EmployeeNew(101, "Alice", 28, "IT", 75000.0),
+                new EmployeeNew(102, "Bob", 35, "HR", 62000.0),
+                new EmployeeNew(103, "Charlie", 42, "Finance", 85000.0),
+                new EmployeeNew(104, "David", 24, "IT", 92000.0),
+                new EmployeeNew(105, "Eva", 30, "Marketing", 68000.0),
+                new EmployeeNew(106, "Frank", 50, "Finance", 120000.0)
+        );
+
+        Map<String, Double> collect = employees
+                .stream()
+                .collect(Collectors.
+                        groupingBy(EmployeeNew::getDept,
+                                Collectors.averagingDouble(EmployeeNew::getSalary)));
+        Optional<Map.Entry<String, Double>> max = collect.entrySet().stream()
+                .max(Comparator.comparingDouble(Map.Entry::getValue));
+        System.out.println(max);
+    }
+}
